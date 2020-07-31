6e5b47aa38178abb4792235116ca639b83c3fb43
IaghoCristian <iagho.carvalho@computao.ufla.br>
2020-07-31 18:08:19 -0300

    heroku config

--- ormconfig.js
+++ ormconfig.js
@@ -2,10 +2,10 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": [
-    "./src/models/*.ts"
+    "./dist/models/*.js"
   ],
   "migrations": [
-    "./src/database/migrations/*.ts"
+    "./dist/database/migrations/*.js"
   ],
   "cli": {
     "migrationsDir": "./src/database/migrations"
