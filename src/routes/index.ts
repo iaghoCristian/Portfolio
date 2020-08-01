1cfd7b459b6eb89dd7060a0a879884d355f15a38
IaghoCristian <iagho.carvalho@computao.ufla.br>
2020-08-01 20:18:39 -0300

    repair the image link

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,9 +8,9 @@ const routes = Router();
 routes.use('/projects', projectsRouter);
 routes.use('/certificates', certificatesRouter);
 routes.use('//images', express.static(config.imagesDirectory));
-routes.use('/icons', express.static(config.iconsDirectory));
-routes.use('/videos', express.static(config.videosDirectory));
-routes.use('/gifs', express.static(config.gifsDirectory));
-routes.use('/certificates/file', express.static(config.certificatesDirectory));
+routes.use('//icons', express.static(config.iconsDirectory));
+routes.use('//videos', express.static(config.videosDirectory));
+routes.use('//gifs', express.static(config.gifsDirectory));
+routes.use('//certificates/file', express.static(config.certificatesDirectory));
 
 export default routes;
